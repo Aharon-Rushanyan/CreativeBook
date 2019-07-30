c124ede8c46d381507993be369b74a4a8f755228
Home <home@Homes-MacBook-Pro.local>
2019-07-30 16:41:08 +0400

    Books

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Book from './components/book/Book';
 
+
+
+const books = [
+  {
+    title: "Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and.",
+    shortDescription: "Lorem Ipsum has been the industry's standard dummy",
+    startDate: new Date(),
+    endDate: new Date(),
+    comments: ["hhhhh", "kkkk"],
+    rate: 5,
+    status: 1,
+    page: 10,
+    pages: 400,
+    imageUrl:"https://images-eu.ssl-images-amazon.com/images/I/513XpOgZtiL.jpg",
+  },
+  {
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel Garisa Markes",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and.",
+    shortDescription: "Lorem Ipsum has been the industry's standard dummy",
+    startDate: new Date(),
+    endDate: new Date(),
+    comments: ["hhhhh", "kkkk"],
+    rate: 6,
+    status: 1,
+    page: 10,
+    pages: 400,
+    imageUrl:"https://s26162.pcdn.co/wp-content/uploads/2018/02/100_Years_First_Ed_Hi_Res-768x1153.jpg",
+  },
+  {
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel Garisa Markes",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and.",
+    shortDescription: "Lorem Ipsum has been the industry's standard dummy",
+    startDate: new Date(),
+    endDate: new Date(),
+    comments: ["hhhhh", "kkkk"],
+    rate: 6,
+    status: 1,
+    page: 10,
+    pages: 400,
+    imageUrl:"https://s26162.pcdn.co/wp-content/uploads/2018/02/100_Years_First_Ed_Hi_Res-768x1153.jpg",
+  }
+];
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="appContainer">
+        <div className="booksWrapper">
+        {books.map((book, i) => {
+          return <Book key={ i } book={book} />;
+        })}
+        </div>
+      </div>
     </div>
   );
 }
